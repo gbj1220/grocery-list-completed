e7bd38e109fb80410fbb21d3410facea8bbfe1df
Gregory Johnson <3531op@gmail.com>
2021-04-18 15:26:41 -0400

    Added React.createContext to context.js

--- grocery-list-completed/src/context.js
+++ grocery-list-completed/src/context.js
@@ -1,7 +1,4 @@
 import React from "react";
 
-function context() {
-  return <div></div>;
-}
-
-export default context;
+export const GroceryInputContext = React.createContext({});
+export const GroceryItemsContext = React.createContext({});
