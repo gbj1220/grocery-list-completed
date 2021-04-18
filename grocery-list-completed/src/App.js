25986e5b00457db69feb1a078d4f79d30d2b6bb8
gbj1220 <3531op@gmail.com>
2021-04-18 15:32:13 -0400

    Added hard coded data

--- grocery-list-completed/src/App.js
+++ grocery-list-completed/src/App.js
@@ -2,6 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const hardCodedData = [
+    {
+      id: u4(),
+      groceryItem: "bananas",
+    },
+    {
+      id: u4(),
+      groceryItem: "oranges",
+    },
+  ];
+
   return <div className='App'></div>;
 }
 
